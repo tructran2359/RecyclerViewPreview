816eae821892266e318ecea6e2d2c77429ddcef6
tructran2359 <truc.trantrung@2359media.com>
2021-03-05 10:55:20 +0700

    Function to generate data

---
+++ app/src/main/java/com/me/recyclerviewpreview/Story.kt
@@ -0,0 +1,25 @@
+package com.me.recyclerviewpreview
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Story(
+    @SerializedName("nid")
+    val nid: String? = null,
+    @SerializedName("title")
+    val title: String? = null,
+    @SerializedName("description")
+    val description: String? = null,
+    @SerializedName("release_date")
+    val releaseDate: String? = null,
+    @SerializedName("image_url")
+    val imageUrl: String? = null,
+    @SerializedName("category")
+    val category: String? = null,
+    @SerializedName("changed_date")
+    val changedDate: String? = null,
+    @SerializedName("uuid")
+    val uuid: String? = null,
+    @SerializedName("author")
+    val author: String? = null
+)
